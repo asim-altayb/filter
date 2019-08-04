192d6c9f79b6cfc60e52dc092ad4c34e4a0b5591
asim altayb <asomy0909@gmail.com>
2019-08-04 22:53:56 +0200

    update and added filter

--- jobs.java
+++ jobs.java
@@ -2,10 +2,12 @@ package joud.asim.sd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -26,6 +28,7 @@ public class jobs extends AppCompatActivity {
     private JSONArray result;
     Button kh , bh , um;
     job_card card;
+    String key;
     private ArrayList<job_model> models= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,36 @@ public class jobs extends AppCompatActivity {
         kh=(Button) findViewById(R.id.kh_filter);
         bh=(Button) findViewById(R.id.bh_filter);
         um=(Button) findViewById(R.id.um_filter);
+
+        //filter jobs in khartoum
+        kh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                key="الخرطوم";
+                filter(key);
+            }
+        });
+
+        //filter jobs in bahry
+        bh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                key="بحري";
+                filter(key);
+            }
+        });
+
+
+        //filter jobs in khartoum
+        um.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                key="أمدرمان";
+                filter(key);
+            }
+        });
+
+
 
         getData();
 
@@ -74,14 +107,14 @@ public class jobs extends AppCompatActivity {
 
         )
         {
-            @Override
+           /* @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String>  params = new HashMap<String, String>();
                 params.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240");
                 params.put("Cookie", "__test="+cookie.content+"; expires=Friday, January 1, 2038 at 1:55:55 AM; path=/");
 
                 return params;
-            }
+            }*/
         };
         RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getApplication()));
         requestQueue.add(stringRequest);
@@ -118,4 +151,57 @@ public class jobs extends AppCompatActivity {
 	'adve_data'=>$row["adve_data"],
 	'end_data'=>$row["end_data"],
 	'salary'=>$row["salary"]*/
+
+    public void filter(String key)
+    {
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.POST,"http://joud.rf.gd/job_filter.php",
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                        JSONObject j = null;
+                        try {
+                            j = new JSONObject(response);
+                            result = j.getJSONArray("result");
+                            System.out.println(result.toString());
+                            getneeds(result);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Error in volly : "+error.getMessage());
+
+                    }
+
+
+
+                }
+
+        )
+        {
+           /* @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240");
+                params.put("Cookie", "__test="+cookie.content+"; expires=Friday, January 1, 2038 at 1:55:55 AM; path=/");
+
+                return params;
+            }*/
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                 Map<String, String> params=new HashMap<String, String>();
+                 params.put("key",key);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getApplication()));
+        requestQueue.add(stringRequest);
+    }
 }
